ec757b51f463f9e782d3e16bb1dd29c26509ac81
polina.vorontsova <polina.vorontsova.21@mail.ru>
2021-10-19 00:33:24 +0300

    Added one-threaded console application testing and serialization testing

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -1,12 +1,58 @@
-﻿using System;
+﻿using System.Threading;
+using Tracer.Entities;
+using Tracer.Services;
+using Tracer.Services.Impl;
 
 namespace ConsoleApplication
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static readonly ITracer Tracer = new MethodTracer();
+
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            new Program().Inner();
+            new Program().MoreInsideInner();
+
+            var traceResult = Tracer.GetTraceResult();
+
+            ISerializer<TraceResult> jsonSerializer = new TraceResultJsonSerializer();
+            ISerializer<TraceResult> xmlSerializer = new TraceResultXmlSerializer();
+            var xmlResult = xmlSerializer.Serialize(traceResult);
+            var jsonResult = jsonSerializer.Serialize(traceResult);
+
+            IPrinter jsonPrinter = new FilePrinter("../../result.json");
+            IPrinter xmlPrinter = new FilePrinter("../../result.xml");
+            IPrinter consolePrinter = new ConsolePrinter();
+
+            xmlPrinter.Print(xmlResult);
+            jsonPrinter.Print(jsonResult);
+            consolePrinter.Print(xmlResult);
+            consolePrinter.Print(jsonResult);
+        }
+
+        private void Inner()
+        {
+            Tracer.StartTrace();
+            Thread.Sleep(500);
+            InsideInner();
+            InsideInner();
+            Tracer.StopTrace();
+        }
+
+        private void InsideInner()
+        {
+            Tracer.StartTrace();
+            Thread.Sleep(700);
+            MoreInsideInner();
+            Tracer.StopTrace();
+        }
+
+        private void MoreInsideInner()
+        {
+            Tracer.StartTrace();
+            Thread.Sleep(500);
+            Tracer.StopTrace();
         }
     }
 }
