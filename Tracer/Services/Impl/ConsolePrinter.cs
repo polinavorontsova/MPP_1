d2bedf38b432914409a1c8ce640e5714324dd1f1
polina.vorontsova <polina.vorontsova.21@mail.ru>
2021-10-19 00:25:03 +0300

    Added file and console implementaion for printer interface

---
+++ Tracer/Services/Impl/ConsolePrinter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tracer.Services.Impl
+{
+    public class ConsolePrinter : IPrinter
+    {
+        public void Print(string data)
+        {
+            Console.WriteLine(data);
+        }
+    }
+}
---
+++ Tracer/Services/Impl/FilePrinter.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Tracer.Services.Impl
+{
+    public class FilePrinter : IPrinter
+    {
+        private readonly string _filePath;
+
+        public FilePrinter(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public void Print(string data)
+        {
+            File.WriteAllText(_filePath, data, Encoding.UTF8);
+        }
+    }
+}
