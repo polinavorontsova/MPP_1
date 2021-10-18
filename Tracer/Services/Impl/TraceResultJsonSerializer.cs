accc53d4872a0b13d8be50bc6ba44a4858c2b530
polina.vorontsova <polina.vorontsova.21@mail.ru>
2021-10-19 00:26:11 +0300

    Added xml and json serializer implementations

---
+++ Tracer/Services/Impl/TraceResultJsonSerializer.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+using Tracer.Entities;
+
+namespace Tracer.Services.Impl
+{
+    public class TraceResultJsonSerializer : ISerializer<TraceResult>
+    {
+        public string Serialize(TraceResult data)
+        {
+            var options = new JsonSerializerOptions
+            {
+                AllowTrailingCommas = true,
+                IgnoreNullValues = true,
+                WriteIndented = true
+            };
+            return JsonSerializer.Serialize(data, options);
+        }
+    }
+}
---
+++ Tracer/Services/Impl/TraceResultXmlSerializer.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Serialization;
+using Tracer.Entities;
+
+namespace Tracer.Services.Impl
+{
+    public class TraceResultXmlSerializer : ISerializer<TraceResult>
+    {
+        private const string RootElementName = "root";
+
+        public string Serialize(TraceResult data)
+        {
+            var traces = data.Traces.ToList();
+            var xmlSerializer = new XmlSerializer(traces.GetType(), new XmlRootAttribute(RootElementName));
+
+            using var stringWriter = new StringWriter();
+            using var writer = new XmlTextWriter(stringWriter);
+            writer.Formatting = Formatting.Indented;
+            xmlSerializer.Serialize(writer, traces);
+
+            return stringWriter.ToString();
+        }
+    }
+}
